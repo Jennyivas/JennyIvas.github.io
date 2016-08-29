83675c82da8050ead35cb56b50febcdbb5f96051
Jenny Ivas <contact.j.ivas@gmail.com>
2016-08-30 00:12:09 +0100

    Site
    Version 1.1

---
+++ fade.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.fadein img:gt(0)').hide();
+    setInterval(function () {
+        $('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');
+    }, 2000);
+});
