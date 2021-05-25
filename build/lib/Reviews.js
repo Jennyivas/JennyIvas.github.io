94a4a4b8dc2079cbb614857e7616382655f9c64d
Jennyivas <contact.j.ivas@gmail.com>
2021-05-26 00:42:58 +0100

    hopefully catching any errors etc...

---
+++ build/lib/Reviews.js
@@ -0,0 +1,23 @@
+﻿var review = document.getElementById('review');
+var reviewer = document.getElementById('reviewer');
+
+function myFunction(xml) {
+    var tot = parseInt(xml.responseXML.getElementsByTagName("review")[0].childNodes.length - 1);
+    var i = 0;
+
+    function reviews() {
+            review.textContent = xml.responseXML.getElementsByTagName("comment")[i].textContent;
+            reviewer.textContent = xml.responseXML.getElementsByTagName("author")[i].textContent;
+            if (i < (tot - 1)) { i++; } 
+            else { i = 0; }
+    }
+
+    reviews();
+    var id = setInterval(reviews, 5000);
+
+}
+
+function hardset() {
+    review.textContent = "Jenny has helped me learn music in way that I thought I couldn't. Jenny has made me feel welcome and has always been felixible with my lessons as I work shifts. Jenny is an excellent teacher and not to mention a great pianoist herself.";
+    reviewer.textContent = "Charlene Mighten";
+}
---
+++ build/lib/startup.js
@@ -0,0 +1,18 @@
+﻿var xmlhttp;
+
+if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+} else {
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+try {
+    xmlhttp.open("GET", "lib/Reviews.xml", true);    //configures the connection - async set to true
+    xmlhttp.send();                                  //sends out the command to open the connection
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            xmlhttp.onload = myFunction(xmlhttp); }
+    };
+}
+catch(error) {
+    hardset();
+}
