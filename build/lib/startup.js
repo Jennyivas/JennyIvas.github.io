305d953f2e0eaccee40d0a0429a11ee8bf513bf5
Jennyivas <contact.j.ivas@gmail.com>
2021-05-26 00:50:48 +0100

    Add files via upload

--- build/lib/startup.js
+++ build/lib/startup.js
@@ -1,11 +1,11 @@
 ﻿var xmlhttp;
 
-if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-} else {
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-}
 try {
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
     xmlhttp.open("GET", "lib/Reviews.xml", true);    //configures the connection - async set to true
     xmlhttp.send();                                  //sends out the command to open the connection
     xmlhttp.onreadystatechange = function () {
@@ -13,6 +13,22 @@ try {
             xmlhttp.onload = myFunction(xmlhttp); }
     };
 }
-catch(error) {
+catch(err) {
     hardset();
+}
+
+function myFunction(xml) {
+    var tot = parseInt(xml.responseXML.getElementsByTagName("review")[0].childNodes.length - 1);
+    var i = 0;
+
+    function reviews() {
+            review.textContent = xml.responseXML.getElementsByTagName("comment")[i].textContent;
+            reviewer.textContent = xml.responseXML.getElementsByTagName("author")[i].textContent;
+            if (i < (tot - 1)) { i++; } 
+            else { i = 0; }
+    }
+
+    reviews();
+    var id = setInterval(reviews, 5000);
+
 }
