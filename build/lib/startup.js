6d0a9fa1826922799f98b9dc0ffdd74d8a6f0c1d
Jennyivas <contact.j.ivas@gmail.com>
2021-05-26 01:06:40 +0100

    Update startup.js

--- build/lib/startup.js
+++ build/lib/startup.js
@@ -22,10 +22,12 @@ function myFunction(xml) {
     var i = 0;
 
     function reviews() {
-            review.textContent = xml.responseXML.getElementsByTagName("comment")[i].textContent;
-            reviewer.textContent = xml.responseXML.getElementsByTagName("author")[i].textContent;
-            if (i < (tot - 1)) { i++; } 
-            else { i = 0; }
+        var review = document.getElementById('review');
+        var reviewer = document.getElementById('reviewer');
+        review.textContent = xml.responseXML.getElementsByTagName("comment")[i].textContent;
+        reviewer.textContent = xml.responseXML.getElementsByTagName("author")[i].textContent;
+        if (i < (tot - 1)) { i++; } 
+        else { i = 0; }
     }
 
     reviews();
