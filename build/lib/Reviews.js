37842a6152e34a6a7e97fa3c338560ffdc491577
Jennyivas <contact.j.ivas@gmail.com>
2021-05-29 12:31:49 +0100

    Delete Reviews.js

--- build/lib/Reviews.js
+++
@@ -1,7 +0,0 @@
-﻿var review = document.getElementById('review');
-var reviewer = document.getElementById('reviewer');
-
-function hardset() {
-    review.textContent = "Jenny has helped me learn music in way that I thought I couldn't. Jenny has made me feel welcome and has always been felixible with my lessons as I work shifts. Jenny is an excellent teacher and not to mention a great pianoist herself.";
-    reviewer.textContent = "Charlene Mighten";
-}
